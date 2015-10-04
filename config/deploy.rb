643b7c5dfe9bf4f313d8ddfd97db9e01888f207b
Sidnei Pereira <p.sidneis@gmail.com>
2015-10-04 13:53:05 -0300

    alter server port

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 # Change these
-server '198.199.110.146', port: 8001, roles: [:web, :app, :db], primary: true
+server '198.199.110.146', port: 80, roles: [:web, :app, :db], primary: true
 
 set :repo_url,        'git@github.com:psidneis/finapp.git'
 set :application,     'finapp'
