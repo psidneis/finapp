3b69091be17766e491e4f9c95b7e823163fc331a
Sidnei Pereira <p.sidneis@gmail.com>
2015-10-11 19:45:36 -0300

    translate model fields

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,12 @@ module Finapp
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Brasilia'
 
+    I18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :'pt-BR'
+
+
+    config.encoding = "utf-8"
   end
 end
