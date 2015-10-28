963ae1a12e9800bcfd7c5c02457388cfa93282fe
Sidnei Pereira <p.sidneis@gmail.com>
2015-10-28 19:25:02 -0200

    add before action authorize resources

--- app/controllers/launches_controller.rb
+++ app/controllers/launches_controller.rb
@@ -1,4 +1,5 @@
 class LaunchesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_launch, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -1,4 +1,5 @@
 class UserGroupsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_user_group, only: [:show, :edit, :update, :destroy]
 
   respond_to :html
