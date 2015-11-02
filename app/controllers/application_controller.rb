897bbd9ebb8ce38c91dbe072417a75d6367eccb2
Sidnei Pereira <p.sidneis@gmail.com>
2015-11-02 14:28:16 -0200

    fixed permitted parameters sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,10 @@ require "application_responder"
 
 class ApplicationController < ActionController::Base
   include Pundit
-
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   self.responder = ApplicationResponder
   respond_to :html, :json
 
@@ -17,5 +18,13 @@ class ApplicationController < ActionController::Base
     def user_not_authorized
      flash[:error] = 'Você não tem permissão para fazer esta ação'
      redirect_to(request.referrer || root_path)
+    end
+
+    def configure_permitted_parameters
+      ## To permit attributes while registration i.e. sign up (app/views/devise/registrations/new.html.erb)
+      devise_parameter_sanitizer.for(:sign_up) << :name# << :attrb2 
+
+      ## To permit attributes while editing a registration (app/views/devise/registrations/edit.html.erb)
+      devise_parameter_sanitizer.for(:account_update) << :name
     end
 end
