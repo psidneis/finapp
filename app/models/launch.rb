11f3a53ba84edd7f873b0d083677f1b791b25c3c
Sidnei Pereira <p.sidneis@gmail.com>
2015-11-02 19:35:20 -0200

    fixed input field polymorphic associantion

--- app/models/launch.rb
+++ app/models/launch.rb
@@ -6,7 +6,7 @@ class Launch < ActiveRecord::Base
   belongs_to :user
   belongs_to :group
 
-  validates :title, :value, :date, :launchable, :recurrence_type, :recurrence, :launch_type, :category, presence: true
+  validates :title, :value, :date, :recurrence_type, :recurrence, :launch_type, :category, presence: true
   validates :title, length: { in: 2..100 }
   validates :value, numericality: { greater_than_or_equal_to: 0 }
   validates :amount_installment, numericality: { only_integer: true, greater_than_or_equal_to: 1 }
@@ -14,5 +14,13 @@ class Launch < ActiveRecord::Base
   enum recurrence_type: %w(not_recurrence installments recurrence)
   enum recurrence: %w(yearly biannual quarterly bimonthly monthly fortnightly weekly daily)
   enum launch_type: %w(expense income)
+
+  def global_launchable
+    self.launchable.to_global_id if self.launchable.present?
+  end
+
+  def global_launchable=(launchable)
+    self.launchable = GlobalID::Locator.locate launchable
+  end
 
 end
