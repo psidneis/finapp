538190cc1d81b84e66e50df7e3b8bef0e83a234e
Sidnei Pereira <p.sidneis@gmail.com>
2015-11-03 23:12:57 -0200

    v1 of method to generate installments

--- app/models/launch.rb
+++ app/models/launch.rb
@@ -23,4 +23,33 @@ class Launch < ActiveRecord::Base
     self.launchable = GlobalID::Locator.locate launchable
   end
 
+  def generate_installments(options={})
+    date_installment = self.date
+    for i in 1..self.amount_installment
+      installment = self.installments.build
+      installment.date = date_installment
+      installment.value = self.value
+      installment.number_installment = i
+      installment.paid = i.eql?(1) ? self.paid : false
+      installment.save
+      date_installment = self.current_date_installment(date_installment)
+    end
+  end
+
+  def current_date_installment(date_installment)
+    recurrence = {
+      yearly: {count: 1, period: :year}, 
+      biannual: {count: 6, period: :months}, 
+      quarterly: {count: 3, period: :months}, 
+      bimonthly: {count: 2, period: :months}, 
+      monthly: {count: 1, period: :month}, 
+      fortnightly: {count: 15, period: :days}, 
+      weekly: {count: 1, period: :week}, 
+      daily: {count: 1, period: :day}
+    }
+
+    recurrence = recurrence[self.recurrence.to_sym]
+    date_installment + recurrence[:count].send(recurrence[:period])
+  end
+
 end
