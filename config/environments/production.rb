75edb4dd71d424261d9980d81b81e65c57e535b1
Sidnei Pereira <p.sidneis@gmail.com>
2015-11-25 16:40:10 -0200

    add config to mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.secrets.email_username,
+    password: Rails.application.secrets.messages_email_password,
+    domain: "gmail.com",
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
