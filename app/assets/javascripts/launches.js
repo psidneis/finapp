4f17626b83b0ee18027f74ee3208cec1d7dc7748
Éverton Braz da Rocha <evertonb_rocha@hotmail.com>
2015-11-28 05:03:39 -0200

    Ajustes layout form lançamento

--- app/assets/javascripts/launches.js
+++ app/assets/javascripts/launches.js
@@ -13,3 +13,21 @@ $(document).on('change', '.recurrence_type_input', function(){
 	}	
 	$('#recurrence_type_chosen').html(text_chosen);
 });
+
+$(document).on('click', '.paid_input_icon', function(){
+	if($(this).data('paid') == 'not_paid'){
+		$(this).data('paid', 'paid');
+		$(this).removeClass('icon-thumbs-down-alt');
+		$(this).removeClass('text-red');
+		$(this).addClass('icon-thumbs-up-alt');
+		$(this).addClass('text-green');
+		$(this).prop('title', $(this).data('title-not-paid'))
+	}else{
+		$(this).data('paid', 'not_paid');
+		$(this).removeClass('icon-thumbs-up-alt');
+		$(this).removeClass('text-gren');
+		$(this).addClass('icon-thumbs-down-alt');
+		$(this).addClass('text-red');
+		$(this).prop('title', $(this).data('title-paid'))
+	}
+});
